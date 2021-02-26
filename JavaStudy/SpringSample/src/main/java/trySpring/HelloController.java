4cc94b1f52269d8384ab1c598c8ddeff8ad5a92d
akari <amamhrhr@gmail.com>
2021-02-26 19:15:46 +0900

    Java-SpringSample

---
+++ JavaStudy/SpringSample/src/main/java/trySpring/HelloController.java
@@ -0,0 +1,15 @@
+package trySpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HelloController {
+
+	@GetMapping("/hewllo")
+public String getHello() {
+		return "hello";
+	}
+
+}
